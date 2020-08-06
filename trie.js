df2361a7689fd0f818f6a24446a4e1385bd6cb23
nazurudeen23 <58662337+nazurudeen23@users.noreply.github.com>
2020-08-07 00:33:10 +0530

    Create trie.js

---
+++ trie.js
@@ -0,0 +1,89 @@
+/* Trie Data Structure */
+
+// @ts-ignore
+let Node = function () {
+  this.keys = new Map();
+  this.end = false;
+  this.setEnd = function () {
+    this.end = true;
+  };
+  this.isEnd = function () {
+    return this.end;
+  };
+};
+
+let Trie = function () {
+  this.root = new Node();
+
+  this.add = function (input, node = this.root) {
+    if (input.length == 0) {
+      node.setEnd();
+      return;
+    } else if (!node.keys.has(input[0])) {
+      node.keys.set(input[0], new Node());
+      return this.add(input.substr(1), node.keys.get(input[0]));
+    } else {
+      return this.add(input.substr(1), node.keys.get(input[0]));
+    }
+  };
+
+  this.isWord = function (word) {
+    let node = this.root;
+    while (word.length > 1) {
+      if (!node.keys.has(word[0])) {
+        return false;
+      } else {
+        node = node.keys.get(word[0]);
+        word = word.substr(1);
+      }
+    }
+    return node.keys.has(word) && node.keys.get(word).isEnd() ? true : false;
+  };
+
+  this.print = function () {
+    let words = new Array();
+    let search = function (node, string) {
+      if (node.keys.size != 0) {
+        for (let letter of node.keys.keys()) {
+          search(node.keys.get(letter), string.concat(letter));
+        }
+        if (node.isEnd()) {
+          words.push(string);
+        }
+      } else {
+        string.length > 0 ? words.push(string) : undefined;
+        return;
+      }
+    };
+    search(this.root, new String());
+    // @ts-ignore
+    return words.length > 0 ? words : mo;
+  };
+};
+
+// @ts-ignore
+myTrie = new Trie();
+// @ts-ignore
+myTrie.add("ball");
+// @ts-ignore
+myTrie.add("bat");
+// @ts-ignore
+myTrie.add("doll");
+// @ts-ignore
+myTrie.add("dork");
+// @ts-ignore
+myTrie.add("do");
+// @ts-ignore
+myTrie.add("dorm");
+// @ts-ignore
+myTrie.add("send");
+// @ts-ignore
+myTrie.add("sense");
+// @ts-ignore
+console.log(myTrie.isWord("doll"));
+// @ts-ignore
+console.log(myTrie.isWord("dor"));
+// @ts-ignore
+console.log(myTrie.isWord("dorf"));
+// @ts-ignore
+console.log(myTrie.print());
